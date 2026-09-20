9899d94237d94d57435805a13655b6e7597fe421
khiettt <khitam.bakkali.nebkhout@hva.nl>
2026-09-20 12:51:55 +0200

    Informatie in de details toegevoegd #6
    In de details vind je alle informatie voor de juiste "summary" naam

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -40,16 +40,38 @@
         </div>    
         <details class="folder folder--about">
             <summary>About me</summary>
+            <p class="p-folder">Nickname: {person.nickname}</p>
+            <p class="p-folder">Birthdate: {person.birthdate}</p>
+            <p class="p-folder">Born and raised in {person.residency}</p>
+            <p class="p-folder"> Hobbies: {person.fav_hobby} and {person.fav_game}</p>
+            <p class="p-folder">Favorite movie: {person.fav_movie}</p>
             
         </details>
     
         <details class="folder folder--projects">
             <summary>Projects</summary>
+            <p class="p-folder">Semster 1</p>
+            <a href="https://khiettt.github.io/your-tribe-profile-card/">Profilecard sprint 1</a>
+            <a href="https://khiettt.github.io/your-tribe-squad-page/" >Squadpage Sprint 1</a>
+            <a href="https://khiettt.github.io/the-client-website/"> Buurtcampus Sprint 2</a>
+            <a href="https://khiettt.github.io/the-client-website/">Buurtcampus Sprint 4</a>
+            <a href="https://khiettt.github.io/the-startup-responsive-interactive-website/">Werktijden.nl Sprint 6</a>
+
+            <p class="p-folder">Semster 2</p>
+            <a href="https://connect-your-tribe-profile-card-5f3c.onrender.com">Profilecard sprint 7</a>
+            <a href="https://connect-your-tribe-squad-page-yta5.onrender.com">Squadpage sprint 7</a>
+            <a href="https://server-side-rendering-server-side-website-7oew.onrender.com">Milledoni sprint 8</a>
+            <a href="https://user-experience-enhanced-website-zv3j.onrender.com">Milledoni sprint 10</a>
+            <a href="https://proof-of-concept-khiettt-jebq.onrender.com">Tweakers sprint 12</a>
 
         </details>
     
         <details class="folder folder--skills">
         <summary>Skills</summary>
+        <p class="p-folder">HTML</p>
+        <p class="p-folder">CSS</p>
+        <p class="p-folder">JS</p>
+
         </details>
     </div>
 </main>
