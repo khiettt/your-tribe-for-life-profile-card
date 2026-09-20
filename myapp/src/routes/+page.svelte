348e000dd1296011aa8baa867530eb27a3a355c5
khiettt <khitam.bakkali.nebkhout@hva.nl>
2026-09-20 12:44:40 +0200

    Img en svg's toegevoegd #2

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <script>
+    import selfieImg from '$lib/assets/IMG_6255.webp';
+    import goldenStars from '$lib/assets/Starss.svg';
+    import macCover from '$lib/assets/MacCover.svg';
+    import starDeco from '$lib/assets/StarrDeco.svg';
+    import chessImg from '$lib/assets/Chess.svg';
+    import booksImg from '$lib/assets/Bookss.svg';
+    
     let {data} = $props()
     const person = $derived(data.person[0])
 
