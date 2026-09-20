6e2d1aed39af476309fd34faa3b66df83fcb7749
khiettt <khitam.bakkali.nebkhout@hva.nl>
2026-09-20 11:49:07 +0200

    Data weergeven #3

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let {data} = $props()
+    const person = $derived(data.person[0])
+
+</script>
+    <h1> Hii I'm {person.name}...</h1>
+    <p>{person.bio}</p>
